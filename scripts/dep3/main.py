42c2bda04070d0f29a32c6e6ee46041abaaa816a
Paul Tagliamonte <tag@pault.ag>
2015-01-01 20:54:35 -0500

    Initial

---
+++ scripts/dep3/main.py
@@ -0,0 +1,29 @@
+from dionysus.archive import Archive
+from dep3.parse import get_headers
+import os
+
+archive = Archive("http://192.168.1.50/debian")
+
+
+def flatten(path):
+    for root, dirs, files in os.walk(path):
+        for dpath in dirs:
+            yield from report(os.path.join(root, dpath))
+        yield from (os.path.join(root, x) for x in files)
+
+
+def report(path):
+    with open(path, 'rb') as fd:
+        if path.endswith(".patch") or path.endswith(".diff"):
+            return get_headers(fd)
+
+
+def patch(archive, source, dsc):
+    with source.unpack():
+        return {
+            "dep3": list(map(report, flatten("debian/patches")))
+        }
+
+
+
+archive.map("unstable", "main", patch)
