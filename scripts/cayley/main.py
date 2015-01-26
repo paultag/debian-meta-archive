9d50cc01bbdd29149c4f0853c31e846a937e3eb4
Paul Tagliamonte <tag@pault.ag>
2015-01-25 20:22:49 -0500

    swag

--- scripts/cayley/main.py
+++ scripts/cayley/main.py
@@ -13,4 +13,25 @@ def deps(archive, source, dsc):
     return data
 
 
+def cayley(el, data, component, package, version):
+    if el is None:
+        el = ""
+    data = data['Build-Depends']
+    relations = data.get("relations", [])
+    for relation in relations:
+        targets = relation.get("targets", [])
+        for target in targets:
+            package_bd = target.get("package")
+            el += "<{package}> <{relation}> <{target}>\r\n".format(
+                package=package,
+                relation="build_depends",
+                target=package_bd,
+            )
+    return el
+
+
 archive.amap(10, "unstable", "main", deps)
+data = archive.reduce("main", cayley)
+
+with open("build-depends", "w") as fd:
+    fd.write(data)
